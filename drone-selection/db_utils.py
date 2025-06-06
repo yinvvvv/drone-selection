8aeb0368caede407464990517256cc5f33596ca9
ZXN1225 <102020430+ZXN1225@users.noreply.github.com>
2025-06-06 22:39:53 +0800

    Update db_utils.py

--- drone-selection/db_utils.py
+++ drone-selection/db_utils.py
@@ -1,6 +1,4 @@
-def load_data(file_path):
-    """
-    Load the drone dataset from the CSV file.
-    """
-    data = pd.read_csv(file_path)
-    return data
+import pandas as pd
+
+data = pd.read_csv("drone - original.csv")
+
