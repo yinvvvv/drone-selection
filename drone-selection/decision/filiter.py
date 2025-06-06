c8dce90cc70dfcb161075f46cded692e35df6ac5
ZXN1225 <102020430+ZXN1225@users.noreply.github.com>
2025-06-06 21:41:34 +0800

    Update filiter.py

--- drone-selection/decision/filiter.py
+++ drone-selection/decision/filiter.py
@@ -2,9 +2,5 @@ def filter_drones(data, criteria):
     """
     Filter drones based on user input criteria.
     """
-    for key, value in criteria.items():
-        if isinstance(value, (int, float)):
-            data = data[data[key] >= value]
-        elif isinstance(value, str):
-            data = data[data[key] == value]
+  
     return data
