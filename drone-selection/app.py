84b31cd0c91521de29fdf80f4aae2a9b51a73fd0
ZXN1225 <102020430+ZXN1225@users.noreply.github.com>
2025-06-07 11:51:56 +0800

    Add files via upload
    增加过滤后输入权重的界面，以及计算按钮，但三个模型尚未定义

--- drone-selection/app.py
+++ drone-selection/app.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, request, jsonify
 from db_utils import get_drones
 from decision.filiter import filter_drones
-
+from decision.wsm import calculate_wsm
+from decision.ahp import calculate_ahp
+from decision.topsis import calculate_topsis
 app = Flask(__name__)
 
 @app.route('/')
@@ -11,9 +13,22 @@ def index():
 @app.route('/select', methods=['POST'])
 def select():
     criteria = request.json  # Get filter criteria from frontend
-    drones = get_drones(r'C:\Users\wangh\Desktop\drone\drone-selection\drone-selection\drone _original.csv')    # Read all drone data
+    drones = get_drones(r"C:\Users\lenovo\Desktop\drone-selection-main\drone-selection\drone _original.csv")    # Read all drone data
     filtered = filter_drones(drones, criteria)  # Filter drones
     return jsonify({"success": True, "drones": filtered})
+
+@app.route('/rank', methods=['POST'])
+def rank():
+    data = request.json
+    drones = data["drones"]
+    weights = data["weights"]
+
+    # 调用三种模型计算排名
+    wsm_result = calculate_wsm(drones, weights)
+    ahp_result = calculate_ahp(weights, drones)
+    topsis_result = calculate_topsis(drones, weights)
+
+    return jsonify({"success": True, "wsm": wsm_result, "ahp": ahp_result, "topsis": topsis_result})
 
 if __name__ == '__main__':
     app.run(debug=True)
